1251c48062568ea255300b4c02d5f9e64842fcaf
Sanandan <sanandan200@gmail.com>
2023-12-11 22:15:07 +0530

    commit

--- lib/main_screen.dart
+++ lib/main_screen.dart
@@ -71,7 +71,7 @@ class _MainScreenState extends State<MainScreen> {
                   ),
                   child: Center(
                       child: Text(
-                    "Click ME !",
+                    "Click ME !!",
                     style: TextStyle(
                         color: Colors.white,
                         fontWeight: FontWeight.bold,
